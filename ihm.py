c4f340544e877ff7e41e62b069f1eadb90fe426a
xavier <xaviermarty005@gmail.com>
2023-05-10 20:26:45 +0200

    setTaillePlanche

--- ihm.py
+++ ihm.py
@@ -2,13 +2,15 @@ nb_planches = 10
 
 
 def calculPluviometrie(quantite_eau, taille_planche, debit, pression):
+    print("")
     temps = 0
     return temps
 
 
-planche = {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []} # dictionnaire
+# planche = {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []}  # dictionnaire
 
-planches = {"0":planche}
+planches = {"0": {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []}, "1": {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []}}
+
 
 def lireCapteursPlanche(numero_planche):
     etat = False
@@ -17,25 +19,32 @@ def lireCapteursPlanche(numero_planche):
 
 
 def capteursPompe():
-    debit = 4  #m3/h
+    debit = 4  # m3/h
     pression = 4
     return debit, pression
 
 
 def setTaillePlanche(numero_planche, largeur, longueur):
-    print(largeur,longueur)
-    print(planches[str(numero_planche)]["etat"])
+    print(largeur, longueur)
+    planches[str(numero_planche)]["taille"][0] = largeur
+    planches[str(numero_planche)]["taille"][1] = longueur
+    # print(planches[str(numero_planche)])
     pass
 
 
 if __name__ == "__main__":
     while 1:
-        res = input("numero_planche")
-        if (res == "0"):
-            print(" 1 - set taille \n 2 - set pluviometrie \n 3 - HistoriqueHumidites \n 4 - HistoriqueArossage" )
+        keys = list(planches.keys())
+        print("planches existantes : ", keys)
+        res = input("numero_planche ")
+
+        if res in keys:
+            print(
+                " 1 - set taille \n 2 - set pluviometrie \n 3 - HistoriqueHumidites \n 4 - HistoriqueArossage \n 5 - print planches")
             val = input("valeur : ")
             if (val == "1"):
                 largeur = input("largeur : ")
                 longueur = input("longueur : ")
                 setTaillePlanche(int(res), float(largeur), float(longueur))
-
+            if (val == "5"):
+                print(planches)
