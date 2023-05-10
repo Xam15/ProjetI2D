d6cbc033c7d340faa9a98c7015c7c246264d1ca0
xavier <xaviermarty005@gmail.com>
2023-05-10 19:56:33 +0200

    initial state

--- ihm.py
+++ ihm.py
@@ -6,8 +6,9 @@ def calculPluviometrie(quantite_eau, taille_planche, debit, pression):
     return temps
 
 
-planche = {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []}
+planche = {"etat": False, "taille": [0, 0], "humidite": [], "temps_arrosage": []} # dictionnaire
 
+planches = {"0":planche}
 
 def lireCapteursPlanche(numero_planche):
     etat = False
@@ -16,22 +17,25 @@ def lireCapteursPlanche(numero_planche):
 
 
 def capteursPompe():
-    debit = 12
+    debit = 4  #m3/h
     pression = 4
     return debit, pression
 
 
-def setTaillePlanche(numero_planche, largeur, longeur):
+def setTaillePlanche(numero_planche, largeur, longueur):
+    print(largeur,longueur)
+    print(planches[str(numero_planche)]["etat"])
     pass
 
 
 if __name__ == "__main__":
     while 1:
         res = input("numero_planche")
-        if (res == "1"):
-            print("1 - set taille\n2 - set pluviometrie")
+        if (res == "0"):
+            print(" 1 - set taille \n 2 - set pluviometrie \n 3 - HistoriqueHumidites \n 4 - HistoriqueArossage" )
             val = input("valeur : ")
             if (val == "1"):
                 largeur = input("largeur : ")
                 longueur = input("longueur : ")
                 setTaillePlanche(int(res), float(largeur), float(longueur))
+
